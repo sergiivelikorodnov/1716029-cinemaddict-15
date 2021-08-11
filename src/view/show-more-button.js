4fb58e3baa40c0ac914ae58e4a80a1c6b50a5e8f
Sergii Velikorodnov <sergii.velikorodnov@gmail.com>
2021-08-11 16:54:46 +0200

    Testing

--- src/view/show-more-button.js
+++ src/view/show-more-button.js
@@ -9,14 +9,14 @@ const createShowMoreButton = () => (
 export default class ShowMoreButton extends AbstractView {
   constructor() {
     super();
-    this._clickHandler = this._clickHandler.bind(this);
+    //this._clickHandler = this._clickHandler.bind(this);
   }
 
   getTemplate() {
     return createShowMoreButton();
   }
 
-  _clickHandler(evt) {
+  /* _clickHandler(evt) {
     evt.preventDefault();
 
     this._callback.click();
@@ -25,5 +25,5 @@ export default class ShowMoreButton extends AbstractView {
   setClickHandler(_callback) {
     this._callback.click = _callback;
     this.getElement().addEventListener('click', this._clickHandler);
-  }
+  } */
 }
