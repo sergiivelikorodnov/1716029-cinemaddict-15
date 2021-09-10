e62e1345edcd2cd01eed52b38078e48abb4479b2
Sergii Velikorodnov <sergii.velikorodnov@gmail.com>
2021-09-10 13:12:42 +0200

    Added MAX_EXTRA_MOVIES

--- src/const.js
+++ src/const.js
@@ -1,6 +1,8 @@
 export const MOVIES_COUNT_PER_STEP = 10;
 export const MAX_SHORT_DESCRIPTION_LENGTH = 139;
 export const MINUTES_IN_HOUR = 60;
+export const MAX_EXTRA_MOVIES = 2;
+
 export const SortType = {
   DEFAULT: 'default',
   BY_DATE: 'by-date',
--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,16 +1,17 @@
 import dayjs from 'dayjs';
+import { MAX_EXTRA_MOVIES } from '../const';
 
 export const sortTopMoviesList = (allMovies) =>
   allMovies
     .filter((movie) => movie.totalRating > 0)
     .sort((a, b) => a.totalRating < b.totalRating)
-    .slice(0, 2);
+    .slice(0, MAX_EXTRA_MOVIES);
 
 export const sortMostCommentedMoviesList = (allMovies) =>
   allMovies
     .filter((movie) => movie.comments.length > 0)
     .sort((a, b) => a.comments.length < b.comments.length)
-    .slice(0, 2);
+    .slice(0, MAX_EXTRA_MOVIES);
 
 export const sortMoviesByDate = (MovieA, MovieB) => {
   if (MovieB.dueDate === null) {
